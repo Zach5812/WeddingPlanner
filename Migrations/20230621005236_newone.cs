7f99bdbdc47a82d5440a9f512c31a298125ccefd
Zach5812 <zachlouk7@gmail.com>
2023-06-21 08:02:30 -0700

    first

---
+++ Migrations/20230621005236_newone.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeddingPlanner.Migrations
+{
+    public partial class newone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Weddings_UserId",
+                table: "Weddings",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Weddings_Users_UserId",
+                table: "Weddings",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Weddings_Users_UserId",
+                table: "Weddings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Weddings_UserId",
+                table: "Weddings");
+        }
+    }
+}
